ac95de422456d0488d35675a09eadb600d86ff97
Simon <simonschmitt@Laptop-von-Simon.local>
2024-02-15 11:18:43 +0100

    Format file

--- playlist_manager.py
+++ playlist_manager.py
@@ -1,9 +1,11 @@
 import sys
 from utils import CommandHandler
 
+
 def main():
     command_handler = CommandHandler()
     command_handler.handle_command_line_args(sys.argv[1:])
+
 
 if __name__ == "__main__":
     main()
